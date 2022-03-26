3c0641cafc7f9e314101e0569d5286363239486d
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-03-26 22:16:40 +0100

    service section/component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,124 @@
 import "./services.css"
+import {GiCheckMark} from "react-icons/gi";
 
 const Services = () => {
     return(
-       <section id="services">Services</section>
+       <section id="services">
+           <h5>What I Offer</h5>
+           <h2>Services</h2>
+           <div className="container container__services">
+               <article className="service">
+                   <div className="service__head">
+                       <h3>UX Research</h3>
+                   </div>
+
+                   <ul className="service__list">
+                       <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                   </ul>
+               </article>
+
+             <article className="service">
+                   <div className="service__head">
+                       <h3>UI Design</h3>
+                   </div>
+
+                   <ul className="service__list">
+                       <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                   </ul>
+               </article>
+
+             <article className="service">
+                   <div className="service__head">
+                       <h3>Content Creation</h3>
+                   </div>
+
+                   <ul className="service__list">
+                       <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                     <li>
+                         <GiCheckMark className="service__list-icon"/>
+                         <p>Massa eget egestas purus viverra accumsan in scelerisque.</p>
+                       </li>
+                   </ul>
+               </article>
+           </div>
+       </section>
     )
 };
 
